247bc205a6ada75a0e7fe25e9201881dca2e8d48
Thushan Gunawardana <thushangunawardana@gmail.com>
2021-03-12 08:58:13 +0100

    Improvements to data preperation

--- price_dispersion.R
+++ price_dispersion.R
@@ -1,4 +1,4 @@
-install.packages("tidyverse")
+#install.packages("tidyverse")
 library(tidyverse)
 library(ggplot2)
 library(data.table) 
@@ -8,34 +8,38 @@ price_data <- read.csv("../home_assignment_data_pricing.csv")
 #This function filters data based in the product type
 filter_data<- function(data,product_type){
   data %>%
-    group_by(col_to_group_by) %>%
-    filter(category == product_type) %>%
-    na.omit()
+    filter(category == product_type) %>% 
+    group_by(store_id,year,month_numerical, product_id) %>% 
+    summarise_at(vars("log_of_cpi_adjusted_price"), mean)
 }
 
-#Filtering the Ps3 data
+#Filtering the Ps3 data,
 ps3dt<-filter_data(price_data,"Ps3")
 
-#Filtering the Cellphones data
-cellphonesdt<-filter_data(price_data,"Cellphones")
-
-#head(ps3dt)
-#summary(ps3dt)
-#str(ps3dt)
-
-setDT(ps3dt)
-ps3mean_price<-ps3dt[ ,list(price=mean(price)), by=month_numerical]
+ps3dt<-ps3dt[order(ps3dt$store_id,ps3dt$year,ps3dt$month_numerical),]
 
 
-ggplot(ps3mean_price, aes(x = month_numerical, y = price)) +
+#Filtering the Ps3 data and consider store ids between 10000 and 17000
+ps3dt <- ps3dt %>%
+  filter(store_id > 9000, store_id < 17000)
+
+ps3dt$store_id <- factor(ps3dt$store_id)
+
+ps3dt$product_id <- factor(ps3dt$product_id)
+
+#setDT(ps3dt)
+#ps3mean_price<-ps3dt[ ,list(price=mean(price), store_id), by=product_id]
+
+ggplot(ps3dt, aes(x = month_numerical, y = log_of_cpi_adjusted_price, color = store_id)) +
+  xlab("Time")+
+  ylab("Ps3 product mean price")+
   geom_line()+
-  xlab("Moenth")+
-  ylab("Ps3 product mean price")
+  facet_wrap(~year)
 
-ggplot(cellphonesdt, aes(x = date, y = cpi_adjusted_price, color= store_id)) +
+
+ggplot(ps3dt, aes(x = time(log_of_cpi_adjusted_price), y = log_of_cpi_adjusted_price, color = store_id)) +
+  xlab("Time")+
+  ylab("Ps3 product mean price")+
+  geom_point()+
   geom_line()+
-  xlab("Dates")+
-  ylab("Cellphones product price")
-
-
-
+  facet_wrap(~year)

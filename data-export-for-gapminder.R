ddfd008bb689aba5a506ad379c43b7880f2aab27
Thushan Gunawardana <thushangunawardana@gmail.com>
2021-03-23 03:38:02 +0100

    Improvements

---
+++ data-export-for-gapminder.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+library(ggplot2)
+library(data.table) 
+library(scales)
+library(ggridges)
+price_data <- read.csv("home_assignment_data_pricing.csv")
+#data_pricing <-fread("home_assignment_data_pricing.csv")
+#This function filters data based in the product type
+filter_data<- function(data,product_type){
+  data %>% 
+    filter(category == product_type) %>% 
+    group_by(year,month_numerical, week, store_id, product_id)
+}
+
+#Filtering the Ps3 data,
+ps3dt<-filter_data(price_data,"Ps3")
+#ps3dt<-ps3dt %>%                                        
+#  group_by(year,month_numerical,week,product_id, "store"= store_id) %>%                         
+#  summarise_at(vars(price),             
+#               list(mean_price = mean)) 
+#view(ps3dt)
+ps3dt <- ps3dt%>%filter(store_id > 5000, store_id < 17000)
+
+setDT(ps3dt)
+
+ps3dt[,"time"] <- as.character(paste(ps3dt$year,ps3dt$month_numerical,ps3dt$week,ps3dt$store, sep = "-"))
+
+products<-c("1619812", 
+            "2992621",
+            "2992622",
+            "3096664",
+            "3186032",
+            "2719933",
+            "446376"
+)
+
+ps3dt_filtered_products<- ps3dt[ps3dt$product_id %in% products]
+
+
+ps3dt_product_gt20160501_1619812<-ps3dt_filtered_products[product_id=="1619812"]
+
+ps3dt_product_gt20160501_2992621<-ps3dt_filtered_products[product_id=="2992621"]
+
+ps3dt_product_gt20160501_2992622<-ps3dt_filtered_products[product_id=="2992622"]
+
+ps3dt_product_gt20160501_3096664<-ps3dt_filtered_products[product_id=="3096664"]
+
+ps3dt_product_gt20160501_3186032<-ps3dt_filtered_products[product_id=="3186032"]
+
+ps3dt_product_gt20160501_2719933<-ps3dt_filtered_products[product_id=="2719933"]
+
+ps3dt_product_gt20160501_446376<-ps3dt_filtered_products[product_id=="446376"]
+
+
+transform_to_csv<-function(input){
+  store_ids = unique(input$store_id)
+  prod_id<-input$product_is[0]
+  i<-0
+  for (str_id in store_ids) {
+    
+    
+    input1<-input%>%filter(store_id == str_id)%>%select(cpi_adjusted_price,date,store_id)
+    input1[order(input1$date)]
+    
+    column.names<-input1$date
+    
+    row.names<-as.character(str_id)
+    rows<-input1$cpi_adjusted_price
+    
+    result <- matrix(
+      rows,
+      nrow = length(row.names),
+      byrow = TRUE,
+      dimnames = list(
+        row.names,
+        column.names
+      )
+    )
+    
+    file_path=paste(".\\mydataclean_prod_",prod_id,"_stor_",str_id,".csv")
+    write.csv(result,file_path, row.names = TRUE)
+    
+    i<-i+1
+  }
+  
+  return(result)
+}
+
+transform_to_csv(ps3dt_product_gt20160501_1619812)
--- price_dispersion-final.R
+++ price_dispersion-final.R
@@ -4,6 +4,12 @@ library(data.table)
 library(scales)
 library(ggridges)
 library(ggdendro)
+library(NbClust)
+#k means plotting
+library(cluster)
+library(fpc)
+#pv clust
+library(pvclust)
 
 price_data <- read.csv("../home_assignment_data_pricing.csv")
 
@@ -11,8 +17,7 @@ price_data <- read.csv("../home_assignment_data_pricing.csv")
 filter_data<- function(data,product_type){
   data %>%
     filter(category == product_type) %>% 
-    group_by(year,month_numerical, week, weekday, store_id, product_id) #  %>% 
-    #summarise_at(vars("log_of_cpi_adjusted_price"), mean)
+    group_by(year,month_numerical, week, weekday, store_id, product_id)
 }
 
 #Filtering the Ps3 data,
@@ -63,12 +68,18 @@ products<-c("1619812",
 ps3dt_filtered_products<- ps3dt[product_id %in% products]
 
 
-ggplot(ps3dt_filtered_products, aes(x = date, y = log_of_cpi_adjusted_price, color = store_id, group = store_id)) +
+ggplot(ps3dt_filtered_products, aes(x = date, y = log_of_cpi_adjusted_price, color = store_id)) +
   xlab("Date")+
   ylab("Ps3 price")+
   geom_step(size = 1, alpha = 0.5)+
+  labs(title = 'Prices valiations of selected PS3 products')+
   scale_x_date(breaks = "4 month")+
-  theme(axis.text.x = element_text(angle = 90, hjust = 1))+
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1),
+    plot.title = element_text(color="#766E6A", size=13,hjust = 0.5),
+    axis.title.x = element_text(color="#000000", size=10),
+    axis.title.y = element_text(color="#000000", size=10)
+  )+
   facet_wrap(~product_id)
 
 
@@ -83,7 +94,14 @@ ggplot(ps3dt_filtered_products, aes(x = price, y = product_id, fill = stat(x))) 
   labs(title = 'PS3 prices from 2015-05-01 to 2017-01-01')+
   xlab("Item price")+
   ylab("PS3 product id")+
-  theme_ridges()
+  scale_x_continuous(n.breaks = 10)+
+  theme_ridges()+
+  theme(
+    axis.text.x = element_text(angle = 90, hjust = 1),
+    plot.title = element_text(color="#766E6A", size=13,hjust = 0.5),
+    axis.title.x = element_text(color="#000000", size=10,hjust = 0.5),
+    axis.title.y = element_text(color="#000000", size=10,hjust = 0.5)
+  )
 
 
 
@@ -92,13 +110,9 @@ get_mean_prices_sd_by_vendor<-function(input){
     group_by(product_id, "store"= store_id) %>%                         
     summarise_at(vars(price),             
                  list(mean_price = mean)) 
-  
+  print(data$mean_price)
   data$sd<-sd(data$mean_price)
   return(data)
-}
-
-get_sd_and_product_id<- function(input){
-  c(sd(input$mean_price),input$product_id)
 }
 
 
@@ -106,15 +120,17 @@ draw_density_ridges<-function(input,prod_id){
   input<-input[product_id==prod_id]
   title = paste('PS3 item ',prod_id,' prices from \n 2015-05-01 to 2017-01-01')
   ggplot(input, aes(x = price, y = store_id, fill = factor(store_id))) + 
-    geom_density_ridges(alpha = 0.8)+
+    geom_density_ridges(quantile_lines = TRUE, quantiles = 2,alpha = 0.8)+
+    scale_x_continuous(n.breaks = 20)+
     scale_colour_manual(values = input$store_id)+
     labs(title = title, fill = "Store id")+
     xlab("Item price")+
     ylab("Store id")+
     theme(
+      axis.text.x = element_text(angle = 90, hjust = 1),
       plot.title = element_text(color="#766E6A", size=13,hjust = 0.5),
-      axis.title.x = element_text(color="#766E6A", size=10),
-      axis.title.y = element_text(color="#766E6A", size=10)
+      axis.title.x = element_text(color="#000000", size=10),
+      axis.title.y = element_text(color="#000000", size=10)
     )
 }
 
@@ -192,8 +208,8 @@ summerize<-function(input){
 
 clusters.color = c("red","green","blue")
 
-ps3dt_3186032_all<-ps3dt_3186032_new
-ps3dt_3186032_all.scale<-scale(ps3dt_3186032_all$cpi_adjusted_price)
+ps3dt_3186032_all<-summerize(ps3dt_3186032_new)
+ps3dt_3186032_all.scale<-scale(ps3dt_3186032_all$mean_price)
 ps3dt_3186032_all$store_id <- as.character(ps3dt_3186032_all$store_id)
 distances<-dist(ps3dt_3186032_all.scale, method="euclidean")
 clust_prices_all<-hclust(distances,method = "ward.D")
@@ -201,16 +217,17 @@ num_of_clusters = 3;
 group<- cutree(clust_prices_all, k=num_of_clusters)
 seeds_df_cl <- mutate(ps3dt_3186032_all, cluster = group)
 ggplot(seeds_df_cl, aes(x=store_id, y = cpi_adjusted_price, color = factor(cluster)))+
- labs(title = 'PS3 item 1619812 price clusters \n of three levels',  fill = "Store id")+
+ labs(title = 'PS3 item 3186032 price clusters \n of three levels',  fill = "Store id")+
  geom_point()+
  ylab("Cpi adjusted price")+
  xlab("Store id")+
+ scale_y_continuous(n.breaks = 20)+
  facet_wrap(~year)+
   theme(
     axis.text.x = element_text(angle = 90, hjust = 1),
     plot.title = element_text(color="#766E6A", size=13,hjust = 0.5),
-    axis.title.x = element_text(color="#766E6A", size=10),
-    axis.title.y = element_text(color="#766E6A", size=10)
+    axis.title.x = element_text(color="#000000", size=10),
+    axis.title.y = element_text(color="#000000", size=10)
   )
 
 
@@ -221,23 +238,89 @@ ps3dt_3186032_2015<-filter_cols(ps3dt_3186032_new,"2015")
 ps3dt_3186032_2016<-filter_cols(ps3dt_3186032_new,"2016")
 ps3dt_3186032_2017<-filter_cols(ps3dt_3186032_new,"2017")
 
-generate_and_plot_clusters<-function(input, year){
+generate_and_plot_clusters<-function(input, year, clust){
+  
+  if(clust == "KM"){
+    input<-summerize(input)
+    input$store_id <- as.character(input$store_id)
+    input.scale<-scale(input$mean_price)
+    num_of_clusters = 3
+    clust_prices<-kmeans(input.scale, num_of_clusters)
+    #plotcluster(input$store_id, clust_prices$cluster)
+    clusplot(input, clust_prices$cluster, color=TRUE, shade=TRUE, 
+             labels=2, lines=0, 
+             main = paste("The clustering of product 3186032 \n prices in ",year)  )
+  }
+  else if(clust == "HC"){
+    input<-summerize(input)
+    input$store_id <- as.character(input$store_id)
+    input.scale<-scale(input$mean_price)
+    distances<-dist(input.scale, method="euclidean")
+    clust_prices<-hclust(distances,method = "ward.D2")
+    print(clust_prices)
+    title = paste("The clustering of product 3186032 \n prices in ",year)
+    plot(clust_prices,labels = input$store_id,
+         main= title,  xlab = "Store id", sub = NA)
+    num_of_clusters = 3;
+    group<- cutree(clust_prices, k=num_of_clusters)
+    rect.hclust(clust_prices, k= num_of_clusters, border = clusters.color)
+  }
+  else if(clust == "PVC"){
+    input<-summerize(input)
+    input$store_id <- as.character(input$store_id)
+    input.scale<-scale(input$mean_price)
+    result <- pvclust(input.scale, method.dist="cor", 
+                      method.hclust="average", nboot=3)
+
+    result %>% as.dendrogram %>% 
+      set("branches_k_color", k = 2, value = c("purple", "orange")) %>%
+      plot
+    result %>% text
+    result %>% pvrect
+  }
+ 
+}
+
+generate_and_plot_clusters(ps3dt_3186032_2015, "2015", "KM")
+
+generate_and_plot_clusters(ps3dt_3186032_2016, "2016", "KM")
+
+generate_and_plot_clusters(ps3dt_3186032_2017, "2017", "KM")
+
+generate_and_plot_clusters(ps3dt_3186032_2015, "2015", "HC")
+
+generate_and_plot_clusters(ps3dt_3186032_2016, "2016", "HC")
+
+generate_and_plot_clusters(ps3dt_3186032_2017, "2017", "HC")
+
+#generate_and_plot_clusters(ps3dt_3186032_2015, "2015", "PVC")
+
+#generate_and_plot_clusters(ps3dt_3186032_2016, "2016", "PVC")
+
+#generate_and_plot_clusters(ps3dt_3186032_2017, "2017", "PVC")
+
+
+validate_cluster<-function(input){
   input<-summerize(input)
   input$store_id <- as.character(input$store_id)
   input.scale<-scale(input$mean_price)
   distances<-dist(input.scale, method="euclidean")
-  clust_prices<-hclust(distances,method = "ward.D")
-  title = paste("The clustering of product 3186032 \n prices in ",year)
-  plot(clust_prices_2016,labels = input$store_id,
-       main= title,  xlab = "Store id", sub = NA)
-  num_of_clusters = 3;
-  group<- cutree(clust_prices, k=num_of_clusters)
-  rect.hclust(clust_prices, k= num_of_clusters, border = clusters.color)
+  res <-
+    NbClust(
+      diss = distances,
+      distance = NULL,
+      min.nc = 2,
+      max.nc = 6,
+      method = "ward.D",
+      index = "silhouette"
+    )
+  
+  print(paste("All.index -:",res$All.index))
+  print(paste("Best.nc -:",res$Best.nc))
+  print(paste("All.CriticalValues -:",res$All.CriticalValues))
+  print(paste("Best.partition -:",res$Best.partition))
 }
 
-generate_and_plot_clusters(ps3dt_3186032_2015, "2015")
+validate_cluster(ps3dt_3186032_2015)
 
-generate_and_plot_clusters(ps3dt_3186032_2016, "2016")
-
-generate_and_plot_clusters(ps3dt_3186032_2017, "2017")
 
